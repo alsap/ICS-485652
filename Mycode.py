0102e92f4e4719d8486104e04a8f5797a26bf062
Sadovskyi <90486851+Sadovskyi@users.noreply.github.com>
2021-09-19 22:57:19 +0300

    Add files via upload

---
+++ Mycode.py
@@ -0,0 +1,7 @@
+name = input ("Ваше ім'я: ")
+surname = input ("Ваша прізвище: ")
+rikNarodzhenya = int (input("Ваш рік народження: "))
+potochniyRik = int (input("Поточний рік: "))
+age = potochniyRik - rikNarodzhenya 
+if age >= 18:
+    print (name + " " + surname + " ви повнолітній")
